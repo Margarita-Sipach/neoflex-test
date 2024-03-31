b74308012e54f6f4a3ed4a3b424d46512816ccf4
Margarita Sipach <margarita.sipach@gmail.com>
2024-04-01 02:10:16 +0300

    feat: add cart page

--- src/components/pages/CartPage/index.tsx
+++ src/components/pages/CartPage/index.tsx
@@ -1,3 +1,12 @@
+import { CardI } from '../../../data'
+import { CartItem } from './CartItem'
+import { Result } from './Result'
 import cls from './style.module.scss'
 
-export const CartPage = () => <main className={cls.page}>Cart!</main>
+export const CartPage = ({cards}: {cards: CardI[]}) => <main className={cls.page}>
+    <h2 className={cls.title}>Корзина</h2>
+    <div className={cls.content}>
+        <div className={cls.items}>{cards?.length ? cards.map(card => <CartItem {...card}/>) : "Пусто :("}</div>
+        <Result price={1000} className={cls.result}></Result>
+    </div>
+</main>
