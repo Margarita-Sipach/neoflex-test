0a47e5a3d806c8fbce38a7f050be2224b265c21b
Margarita Sipach <margarita.sipach@gmail.com>
2024-04-01 01:07:08 +0300

    feat: add border and shadow classes, update category view

--- src/components/modules/Card/index.tsx
+++ src/components/modules/Card/index.tsx
@@ -1,10 +1,11 @@
+import classNames from 'classnames'
 import { CardI } from '../../../data'
 import { Button, ButtonTheme } from '../../ui/Button'
 import { Price } from '../../ui/Price'
 import { Star } from '../../ui/Star'
 import cls from './style.module.scss'
 
-export const Card = ({img, name, star, price, oldPrice}: CardI) => <div className={cls.card}>
+export const Card = ({img, name, star, price, oldPrice}: CardI) => <div className={classNames('border', cls.card)}>
     <img src={img} alt="" className={cls.img} />
     <div className={cls.info}>
         <div className={cls.col}>
--- src/components/modules/Footer/index.tsx
+++ src/components/modules/Footer/index.tsx
@@ -2,8 +2,9 @@ import cls from './style.module.scss'
 import { Logo } from '../../ui/Logo';
 import { Nav } from './Nav';
 import { Socials } from './Socials';
+import classNames from 'classnames';
 
-export const Footer = () => <footer className={cls.footer}>
+export const Footer = () => <footer className={classNames('shadow', cls.footer)}>
     <Logo className={cls.logo}/>
     <Nav className={cls.nav}/>
     <Socials className={cls.socials}/>
