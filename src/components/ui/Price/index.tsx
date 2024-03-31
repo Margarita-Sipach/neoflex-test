b57a3587fe75e06c07928d9cd04962c53855e693
Margarita Sipach <margarita.sipach@gmail.com>
2024-04-01 01:52:28 +0300

    feat: add new price themes

--- src/components/ui/Price/index.tsx
+++ src/components/ui/Price/index.tsx
@@ -1,13 +1,20 @@
 import classNames from 'classnames';
 import cls from './style.module.scss'
 
+export enum PriceTheme {
+    primary = 'primary',
+    secondary = 'secondary',
+    dark = 'dark'
+}
+
 interface PriceProps{
     className?: string
     price: number
     oldPrice?: number
+    theme: PriceTheme
 }
 
-export const Price = ({className, price, oldPrice}: PriceProps) => <div className={classNames(cls.wrapper, className)}>
-    <span className={cls.price}>{price} ₽</span>
+export const Price = ({className, price, oldPrice, theme = PriceTheme.secondary}: PriceProps) => <div className={classNames(cls.wrapper, className)}>
+    <span className={classNames(cls[theme], cls.price)}>{price} ₽</span>
     {oldPrice && <span className={cls.oldPrice}>{oldPrice} ₽</span>}
 </div>
