ea18978c022be954fa320fcf7ba6cc828770e98f
Margarita Sipach <margarita.sipach@gmail.com>
2024-03-31 23:00:43 +0300

    feat: add classname for logo

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { Routes } from "../../../lib/routes";
 import cls from './style.module.scss'
+import classNames from "classnames";
 
-export const Logo = () => <Link to={Routes.main} className={cls.logo}>QPICK</Link>
+export const Logo = ({className}: {className: string}) => <Link to={Routes.main} className={classNames(cls.logo, className)}>
+    QPICK
+</Link>
